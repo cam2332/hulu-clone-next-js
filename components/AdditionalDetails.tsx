c2ee88e8577c8dc37bb3e0cc6d6b0356c4529379
cam2332 <kam2332@gmail.com>
2021-05-30 14:04:11 +0200

    Change cast, crew, genre lists to have clickable items

--- components/AdditionalDetails.tsx
+++ components/AdditionalDetails.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import FlipMove from 'react-flip-move'
 import MovieData from '../types/MovieData'
 import MovieDetailsData from '../types/MovieDetailsData'
@@ -19,7 +20,14 @@ function AdditionalDetails({
   credits: MovieTvShowCreditsData
   }): JSX.Element {
   const BASE_URL = 'https://image.tmdb.org/t/p/original'
+  const router = useRouter()
   const [tab, setTab] = useState(0)
+  const [castListExpanded, setCastListExpanded] = useState(false)
+  const [crewListExpanded, setCrewListExpanded] = useState(false)
+  const releaseYear = (movie as MovieDetailsData).release_date &&
+    (movie as MovieDetailsData).release_date.substring(0, 4) ||
+    (movie as TvShowDetailsData).first_air_date &&
+    (movie as TvShowDetailsData).first_air_date.substring(0, 4)
 
   return (
     <div className="h-full w-full bg-[#06202A]">
@@ -93,46 +101,87 @@ function AdditionalDetails({
         </div>
       }
       {tab === 2 &&
-        <div className='px-10 md:px-20 space-y-4 md:space-y-8 py-8 md:py-16'>
+        <div className='px-10 md:px-20 space-y-4 md:space-y-6 py-8 md:py-16'>
           <p className='text-white font-bold md:text-xl'>
               About this movie
             </p>
             <p className='text-white font-bold text-2xl md:text-5xl'>
               {movie.title || movie.original_title}
           </p>
-          <div className='flex space-x-2'>
-            <p className='font-bold text-gray-400 md:text-xl'>
-            Starring:
+          <div className='flex flex-wrap items-center space-x-3 '>
+            <p
+              onClick={() => setCastListExpanded(!castListExpanded)}
+              className='font-bold additional-details-grey-text'>
+              Starring:
             </p>
-            <p className='text-gray-400 md:text-xl'>
-              {credits.cast.slice(0, 2).map(
-                (cast, index) =>
-                  (index ? ', ' : '') + (cast.name || cast.original_name)
-              )}
-            </p>
+            {!castListExpanded && credits.cast.slice(0, 2).map(
+              (cast, index) =>
+                <p
+                  key={index}
+                  onClick={() => router.push(`/?castId=${cast.id}`)}
+                  className='additional-details-grey-text'>
+                  {cast.name || cast.original_name}
+                </p>
+            )}
+            {castListExpanded && credits.cast.map(
+              (cast, index) =>
+                <p
+                  key={index}
+                  onClick={() => router.push(`/?castId=${cast.id}`)}
+                  className='additional-details-grey-text'>
+                  {cast.name || cast.original_name}
+                </p>
+            )}
           </div>
           <div className='flex space-x-2'>
-            <p className='font-bold text-gray-400 md:text-xl'>
-            Directors:
+            <p
+              onClick={() => setCrewListExpanded(!crewListExpanded)}
+              className='font-bold additional-details-grey-text'>
+              Directors:
             </p>
-            <p className='text-gray-400 md:text-xl'>
-              {credits.crew.filter((crew) => crew.job === 'Producer').slice(0, 2).map(
+            {!crewListExpanded && credits.crew.
+              filter((crew) => crew.job === 'Director').slice(0, 2).map(
                 (crew, index) =>
-                  (index ? ', ' : '') + (crew.name || crew.original_name)
-              )}
-            </p>
+                  <p
+                    key={index}
+                    onClick={() => router.push(`/?crewId=${crew.id}`)}
+                    className='additional-details-grey-text'>
+                    {crew.name || crew.original_name}
+                  </p>
+            )}
+            {crewListExpanded && credits.crew.
+              filter((crew) => crew.job === 'Director').map(
+                (crew, index) =>
+                  <p
+                    key={index}
+                    onClick={() => router.push(`/?crewId=${crew.id}`)}
+                    className='additional-details-grey-text'>
+                    {crew.name || crew.original_name}
+                  </p>
+            )}
         </div>
         {/* change to div and make list of links to genres */}
-        <p className='flex items-center text-gray-400 
-          font-bold md:text-xl'>
+        <div className='flex'>
           {movie.genres && movie.genres.length > 0 &&
-            `${movie.genres.map(genre => genre.name + ' • ').join('')}`}{' '}
-          {(movie as MovieDetailsData).release_date &&
-            (movie as MovieDetailsData).release_date.substring(0, 4) ||
-          (movie as TvShowDetailsData).first_air_date &&
-            (movie as TvShowDetailsData).first_air_date.substring(0, 4)}{' '}
-        </p>
+            movie.genres.map((genre, index) =>
+              <div key={index} className='flex'>
+                <p
+                  onClick={() => router.push(`/?genre=${genre.id}`)}
+                  className='additional-details-grey-text'>
+                  {genre.name}
+                </p>
+                <p className='flex items-center text-gray-400 md:text-lg'>
+                  &nbsp;•&nbsp;
+                </p>
+              </div>
+            )}
+          <p
+            onClick={() => router.push(`/?release_year=${releaseYear}`)}
+            className='additional-details-grey-text'>
+            {releaseYear}
+          </p>
         </div>
+      </div>
       }
     </div>
   )
